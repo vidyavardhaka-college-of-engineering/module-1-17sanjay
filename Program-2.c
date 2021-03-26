ba6b7308996d5fe9cbc6500fb3a647d4f5bb114a
17sanjay <kvsanjay17@gmail.com>
2021-03-26 10:06:25 +0000

    done

--- Program-2.c
+++ Program-2.c
@@ -6,12 +6,12 @@ int main() {
     int number1,number2,sum; // correct the code
     
     printf("Enter two integers:");
-    scanf("%d%d,&number1,&number2);
+    scanf("%d%d" , &number1, &number2);
 
     // calculating sum
     sum=number1+number2;      // insert the missing code
     
-    printf("%d,sum );   // correct the code
+    printf("%d+%d=%d",number1, number2, sum );   // correct the code
     return 0;
 }
 
